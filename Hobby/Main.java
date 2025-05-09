6a82ef226a0e5eda019d88ff9b47bfb9e5590ec9
Keni <petryakov8@gmail.com>
2025-05-10 00:58:02 +0400

    Recursive

--- Hobby/Main.java
+++ Hobby/Main.java
@@ -12,44 +12,41 @@ public class Main {
 
     public static void compare(int day) {
         System.out.println("=== Day " + day + " ===");
-        int[] startNumbers = { 21, 1, 20, 23 };
+        int[] startNumbers = {21, 1, 20, 23};
         int iterative = chooseHobbyIterative(startNumbers, day);
-        int recursive = chooseHobbyRecursive(startNumbers, day, new int[day + 4]); // Инициализация массива memory
+        int recursive = chooseHobbyRecursive(startNumbers, day, new int[day + 1]);
         System.out.println("Iterative = " + iterative + " | Recursive = " + recursive);
         System.out.println();
     }
 
     public static int chooseHobbyRecursive(int[] startNumbers, int day, int[] memory) {
-
         if (day < 4) {
             return startNumbers[day];
         }
-
         if (memory[day] != 0) {
             return memory[day];
         }
 
         int prev = chooseHobbyRecursive(startNumbers, day - 1, memory);
         int prePrePrev = chooseHobbyRecursive(startNumbers, day - 3, memory);
-        int result = (prev * prePrePrev) % 10 + 1;
 
+        int result = (prev * prePrePrev) % 10 + 1;
         memory[day] = result;
         return result;
     }
 
     public static int chooseHobbyIterative(int[] startNumbers, int day) {
         List<Integer> numbers = new ArrayList<>();
-        numbers.add(startNumbers[0]);
-        numbers.add(startNumbers[1]);
-        numbers.add(startNumbers[2]);
-        numbers.add(startNumbers[3]);
+        for (int i = 0; i < 4; i++) {
+            numbers.add(startNumbers[i]);
+        }
 
-        for (int d = 4; d <= day + 3; d++) {
+        for (int d = 4; d <= day; d++) {
             int prev = numbers.get(d - 1);
             int prePrePrev = numbers.get(d - 3);
             numbers.add((prev * prePrePrev) % 10 + 1);
         }
 
-        return numbers.get(day + 3);
+        return numbers.get(day);
     }
 }
